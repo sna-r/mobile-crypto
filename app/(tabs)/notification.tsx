743ca754db48a666e849ba386a763f4434f9554c
SNyAvo <sitrakanyavorabenaivo@gmail.com>
2025-02-06 10:30:07 +0300

    [adding tabs]

---
+++ app/(tabs)/notification.tsx
@@ -0,0 +1,7 @@
+import { View, Text } from "react-native";
+
+export default function Notification(){
+    return <View>
+        <Text> This is the notification page </Text>
+    </View>
+}
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,7 @@
+import { View ,Text} from "react-native";
+
+export default function Profile(){
+    return <View>
+            <Text> This is the profile page </Text>
+        </View>
+}
